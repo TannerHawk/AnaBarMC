e295d1d2d5674fe31ff9636ee108430e896716a8
TannerHawk <tanner.hawk.16@cnu.edu>
2020-02-12 11:18:12 -0500

    new temp branch

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -23,8 +23,6 @@ class DetectorConstruction : public G4VUserDetectorConstruction
   ~DetectorConstruction();
 
   G4VPhysicalVolume* Construct();
-  G4VPhysicalVolume* BarConstruction(G4Material* Pscint,G4LogicalVolume* expHall_log,G4Material* Glass);
-  G4VPhysicalVolume* FingerConstruction(G4Material* Pscint,G4LogicalVolume* expHall_log,G4Material* Glass);
 
   void UpdateGeometry();
 
